8d0275c2a21253c1b8cb1f021287b35b00d2cc2b
riko <e.lorand35@gmail.com>
2018-02-09 22:42:15 +0300

    arch linux install v1

---
+++ arch_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ pacman -S libreoffice firefox  clementine  kodi qbittorrent vlc mpv lxc vim thunderbird synapse redshift gimp fail2ban network-manager-applet openssh-server
+ systemctl enable sshd.service
+
