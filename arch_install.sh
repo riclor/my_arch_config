70b83c65b935cb799ddabb72fcf3d657a9f1eafb
riko <e.lorand35@gmail.com>
2018-02-12 12:22:51 +0300

    maj

--- arch_install.sh
+++ arch_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
- pacman -S libreoffice firefox  clementine  kodi qbittorrent vlc mpv lxc vim thunderbird synapse redshift gimp fail2ban network-manager-applet openssh-server
- systemctl enable sshd.service
+PACKAGES = "libreoffice firefox  clementine  kodi qbittorrent vlc mpv lxc vim thunderbird synapse redshift gimp fail2ban network-manager-applet openssh-server"
 
+pacman -Syu
+
+#pacman -Qqe > pkglist.txt to make that list
+pacman -S - < arch_pkglist.txt
+systemctl enable sshd.service 
