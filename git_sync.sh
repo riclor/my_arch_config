2f4f417703be0dcf3468933043c0fc884aafbd98
riko <e.lorand35@gmail.com>
2018-02-12 21:20:35 +0300

    maj2

--- git_sync.sh
+++ git_sync.sh
@@ -3,3 +3,4 @@
 cp /root/.config/i3/config /root/mygit/i3/config
 cp /root/.bashrc /root/mygit/linux_profile/.bashrc
 cp /root/.bash_profile /root/mygit/linux_profile/.bash_profile
+cp /etc/default/tlp /root/mygit/config_files/tlp
